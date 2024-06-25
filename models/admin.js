876a7f636cb1b45f51c6c67aff185e69bd99c35a
Ajitesh Kumar Singh <Ajitesh.Kumar@iiitb.ac.in>
2024-06-26 02:09:14 +0530

    Upgraded the database Models

--- models/admin.js
+++ models/admin.js
@@ -1,9 +1,42 @@
 const mongoose = require('mongoose');
 
 const adminSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    email: { 
+        type: String, 
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+    }},
+
+    username: { 
+        type: String, 
+        required: true, 
+        unique: true ,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isAlphanumeric(value)) {
+                throw new Error('Username is invalid')
+            }
+        }
+    },
+
+    password: { 
+        type: String, 
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
+            }
+        },
+    }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Admin', adminSchema);
--- models/user.js
+++ models/user.js
@@ -1,10 +1,43 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    verified: { type: Boolean, default: false },
+    email: {
+        type: String,
+        required: true, 
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+
+    username: {
+        type: String,
+        required: true, 
+        unique: true 
+    },
+
+    password: { 
+        type: String, 
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
+            }
+        }
+    },
+
+    verified: { 
+        type: Boolean,
+         default: false 
+    },
+
     location: String,
     age: Number,
     work: String,
@@ -12,7 +45,6 @@ const userSchema = new mongoose.Schema({
     description: String,
     otp: String
 }, { timestamps: true });
-
 
 
 module.exports = mongoose.model('User', userSchema);
